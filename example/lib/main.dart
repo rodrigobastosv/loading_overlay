96affca3e8af18c695978e20423b5b816d5d9233
rodrigobastosv <rodrigobastosv@gmail.com>
2020-03-12 14:42:17 -0300

    Adjusting import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:loadingoverlay/loading_overlay.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
 void main() => runApp(MyApp());
 
