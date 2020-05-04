17cb6ad23d0a58148d928e8513019d6d47b12e0b
Rodrigo <rodrigobastosv@gmail.com>
2020-05-03 21:57:56 -0300

    adding global loader overlay

--- lib/loader_overlay.dart
+++ lib/loader_overlay.dart
@@ -1,3 +1,4 @@
+export 'src/global_loader_overlay.dart';
 export 'src/loader_overlay.dart';
 export 'src/overlay_controller_widget.dart';
 export 'src/overlay_controller_widget_extension.dart';
---
+++ lib/src/global_loader_overlay.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../loader_overlay.dart';
+
+class GlobalLoaderOverlay extends StatelessWidget {
+  GlobalLoaderOverlay({
+    @required this.child,
+    this.useDefaultLoading,
+    this.overlayColor,
+    this.overlayOpacity,
+    this.overlayWidget,
+    this.textDirection = TextDirection.ltr,
+  }) : assert(child != null);
+
+  final Widget child;
+  final bool useDefaultLoading;
+  final Color overlayColor;
+  final double overlayOpacity;
+  final Widget overlayWidget;
+  final TextDirection textDirection;
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: textDirection,
+      child: LoaderOverlay(
+        useDefaultLoading: useDefaultLoading,
+        overlayColor: overlayColor,
+        overlayOpacity: overlayOpacity,
+        overlayWidget: overlayWidget,
+        child: child,
+      ),
+    );
+  }
+}
